553a7263705a67c6f02dd16fa00631ec1b316029
Shagun Shukla <shagunshukla4812@gmail.com>
2025-03-16 16:14:40 +0530

    modified:   insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,21 +1,27 @@
+
+
 def insertion_sort(arr):
+    # Insertion Sort
+    # Best Time Complexity: O(n) - Example: [1, 2, 3, 4, 5]
+    # Worst Time Complexity: O(n^2) - Example: [5, 4, 3, 2, 1]
     n = len(arr)
     for i in range(1, n):
         key = arr[i]
-        j = i-1
+        j = i - 1
         while j >= 0 and key < arr[j]:
-            arr[j+1] = arr[j]
+            arr[j + 1] = arr[j]
             j -= 1
-        arr[j+1] = key
+        arr[j + 1] = key
     return arr
 
 def main():
-    print("Enter the elements of the array: ")  
-    arr =[]
+    print("Enter the elements of the array: ")
+    arr = []
     for i in range(5):
         element = int(input())
-        arr.append(element)     
+        arr.append(element)
     result = insertion_sort(arr)
-    print("The sorted array is: ", result)          
+    print("The sorted array is: ", result)
+
 if __name__ == "__main__":
-    main()  
+    main()
