3e05bfd4eb921316274fd05f52c21efb6f87df96
Shagun Shukla <shagunshukla4812@gmail.com>
2025-03-16 16:01:36 +0530

    new file:   merge_sort.py

---
+++ merge_sort.py
@@ -0,0 +1,37 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+
+def main():
+    print("Enter the elements of the array: ")
+    arr = []
+    for i in range(5):
+        element = int(input())
+        arr.append(element)
+    result = merge_sort(arr)
+    print("The sorted array is: ", result)
+
+if __name__ == "__main__":
+    main()
