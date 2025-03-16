309c35cf8339488df76c20ff016e3f842bb56500
Shagun Shukla <shagunshukla4812@gmail.com>
2025-03-16 16:13:34 +0530

    modified:   bubble.py

--- bubble.py
+++ bubble.py
@@ -1,14 +1,17 @@
 def bubble_sort(arr):
+    # Bubble Sort
+    # Best Time Complexity: O(n) - Example: [1, 2, 3, 4, 5]
+    # Worst Time Complexity: O(n^2) - Example: [5, 4, 3, 2, 1]
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-    return arr  
+    return arr
 
 def main():
     print("Enter the elements of the array: ")
-    arr =[]
+    arr = []
     for i in range(5):
         element = int(input())
         arr.append(element)
