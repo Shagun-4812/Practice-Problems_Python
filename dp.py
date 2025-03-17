8078a9d6622e39b93d1d37d19201534ec88bb764
Shagun Shukla <shagunshukla4812@gmail.com>
2025-03-17 11:13:32 +0530

    new file:   dp.py

---
+++ dp.py
@@ -0,0 +1,7 @@
+def fibonacci_recursive(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+print(fibonacci_recursive(10)) 
