f764a438e8382e7d8253664d5508257057ba6d79
Shagun Shukla <shagunshukla4812@gmail.com>
2025-03-18 10:24:39 +0530

    modified:   rod_problem.py

--- rod_problem.py
+++ rod_problem.py
@@ -5,8 +5,8 @@ class rod(object):
         self.S = [[0] * (n + 1) for _ in range(len(p) + 1)]
 
     def solve(self):
-        for i in range(1, len(self.p) + 1):  # Fix range to include all prices
-            for j in range(self.n + 1):  # Iterate over rod lengths
+        for i in range(1, len(self.p) + 1):  
+            for j in range(self.n + 1):  
                 if i <= j:
                     self.S[i][j] = max(self.S[i - 1][j], self.p[i - 1] + self.S[i][j - i])
                 else:
@@ -14,9 +14,19 @@ class rod(object):
 
     def show_result(self):
         print("The maximum value is", self.S[len(self.p)][self.n])
+        column_index=self.n;
+        row_index=len(self.p)-1;
+        while column_index>0 or row_index>0:
+            if self.S[row_index][column_index]==self.S[row_index-1][column_index]:
+                row_index=row_index-1
+            else:
+                print("We take piece of length",row_index+1,"m")
+                column_index=column_index-row_index-1
+
+
 
 def main():
-    r = rod(5, [2, 5, 7, 3, 9])
+    r = rod(6, [0,2, 5, 7, 3, 9])
     r.solve()
     r.show_result()
 
