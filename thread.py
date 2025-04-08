7f642bc5798e28b94d6d6b39fbb95df56154bff9
Shagun Shukla <shagunshukla4812@gmail.com>
2025-04-08 10:19:41 +0530

    modified:   thread.py

--- thread.py
+++ thread.py
@@ -1,11 +1,11 @@
 import threading
 import time 
 def func1():
-    for i in range(50):
+    for i in range(500):
         print("anything")
         
 def func2():
-    for i in range(50):
+    for i in range(500):
         print("something")
 
 t1= threading.Thread(target=func1, args=[])
