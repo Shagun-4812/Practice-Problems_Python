c5897a0ca961ec62ae23b20082f1a8e4a14f4752
Shagun Shukla <shagunshukla4812@gmail.com>
2025-04-15 10:02:56 +0530

    modified:   lock3.py

--- lock3.py
+++ lock3.py
@@ -5,18 +5,17 @@ import random
 lock = threading.Lock()
 
 def first_person(l):
-    l.acquire()  # Acquire the lock
     print("Person one occupies the room")
-    time.sleep(1)  # Simulate time spent in the room
+    time.sleep(1)  
     print("Person one leaves the room")
-    l.release()  # Release the lock
+    l.release()  
     
 def second_person(l):
-    l.acquire()  # Acquire the lock
+    l.acquire()  
     print("Person two occupies the room")
-    time.sleep(1)  # Simulate time spent in the room
+    time.sleep(1) 
     print("Person two leaves the room")
-    l.release()  # Release the lock
+    l.release()  
     
 t1 = threading.Thread(target=first_person, args=(lock,))
 t2 = threading.Thread(target=second_person, args=(lock,))
