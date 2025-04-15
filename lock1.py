f833cd3e1fb0354d41b7e05da399bd7b6f04f1af
Shagun Shukla <shagunshukla4812@gmail.com>
2025-04-15 10:03:36 +0530

    modified:   lock1.py

--- lock1.py
+++ lock1.py
@@ -2,15 +2,15 @@ import threading
 import time
 
 count = 0
-lock = threading.Lock()  # Lock for thread safety
+lock = threading.Lock()  
 
 def thread_function(num_list):
     global count
-    with lock:  # Ensuring only one thread modifies count at a time
+    with lock:  
         for i in num_list:
             count += i
 
-# Creating and starting threads properly
+
 t1 = threading.Thread(target=thread_function, args=([1, 2, 3, 4],))
 t2 = threading.Thread(target=thread_function, args=([5, 6, 7, 8],))
 t3 = threading.Thread(target=thread_function, args=([9, 10, 11, 12],))
@@ -21,7 +21,7 @@ t2.start()
 t3.start()
 t4.start()
 
-# Waiting for threads to finish
+
 t3.join()
 print("Thread 3 finished")
 t2.join()
