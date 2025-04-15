412a01aa0027bec23184af9caa1c239b0e6a9ab7
Shagun Shukla <shagunshukla4812@gmail.com>
2025-04-15 09:50:18 +0530

    modified:   lock2.py 	new file:   lock3.py

--- lock2.py
+++ lock2.py
@@ -1,2 +1,18 @@
+import threading
+import time
 def first_person():
+    print("person one occupies the room")
+    time.sleep(1)
+    print("person one leaves the room")
     
+    
+def second_person():
+    print("person two occupies the room")
+    time.sleep(1)
+    print("person two leaves the room")
+    
+    
+t1= threading.Thread(target=first_person)
+t2= threading.Thread(target=second_person)
+t1.start()
+t2.start()
