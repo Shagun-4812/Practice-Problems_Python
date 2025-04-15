0caa40b6dbfdbc3c90476ebfb7dd8325b180e5d2
Shagun Shukla <shagunshukla4812@gmail.com>
2025-04-15 10:24:50 +0530

    new file:   lock5.py

---
+++ lock5.py
@@ -0,0 +1,18 @@
+import threading
+
+import time
+
+sem= threading.Semaphore(2)
+
+def worker(name):
+    print(f"{name} is waiting to acquire the semaphore")
+    with sem:
+        print(f"{name} has acquired the semaphore")
+        time.sleep(2)  
+        print(f"{name} is releasing the semaphore")
+for i in range(5):
+    t = threading.Thread(target=worker, args=(f"Thread-{i}",))
+    t.start()
+    
+    
+    
